172a457d65c647989c049acec30ab0fae01da982
EvgeniyOvch <ovchinnicov@gmail.com>
2022-12-28 11:57:07 +0300

    Stat

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,4 +14,15 @@ public class SalesManager {
         }
         return max;
     }
+    public int aver() {
+        int max = 0;
+        int min = sales[0];
+        int aver = 0;
+        for (int sale : sales) {
+            aver += sale;
+            if (sale > max) max = sale;
+            if (sale < min) min = sale;
+        }
+        return aver = (aver - min - max) / (sales.length - 2);
+    }
 }
